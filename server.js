a14ed105cb9a2df958ba98b1ce51fcb222eb2302
jamaludheen kp <jamaljazz1998@gmail.com>
2023-11-17 10:06:32 +0530

    node js port changed 3000 to 8080

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(cors())
 
 app.use(express.json());
 app.use("/", router);
-app.listen(3000, () => console.log("Server is running on port "));
+app.listen(8080, () => console.log("Server is running on port "));
 
 const contactEmail = nodemailer.createTransport({
   service: "gmail",
