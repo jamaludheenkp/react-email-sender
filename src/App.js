70dea82e19fd984fb25858dcd1daf61132363746
jamaludheen kp <jamaljazz1998@gmail.com>
2023-11-24 12:01:51 +0530

    some updates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import ContactForm from './components/ContactForm';
 import './App.css';
+// import { Route, Router, Routes } from 'react-router';
+// import AboutUs from './components/AboutUs';
+// import Home from './components/Home';
 
 function App() {
   return (
-    <ContactForm/>
+    
+    // <Router>
+      <ContactForm/>
+      // {/* <Routes>
+      //   <Route path='/' element={<Home/>}/>
+      //   <Route path='/about' element={<AboutUs/>}/>
+      //   <Route path='/contact' element={<ContactForm/>}/> 
+      // </Routes> */}
+
+    // </Router>
+
   );
 }
 
